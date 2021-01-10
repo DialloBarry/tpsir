cb08764aa7ca41b6869aa4e10350acba915ecf46
DialloBarry <safietoudiallo9@gmail.com>
2021-01-11 00:30:54 +0100

    integration continue

---
+++ src/main/java/tp1/miage/sir/TP1sir/App.java
@@ -0,0 +1,15 @@
+package tp1.miage.sir.TP1sir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+
+{
+	private int code;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
